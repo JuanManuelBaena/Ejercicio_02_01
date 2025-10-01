af40b9be6396922d3e653e3147d0e336650931ef
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-10-01 13:59:18 +0200

    Ejercicio 1 tema 2 finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int x,y = 0;
+        x = 144;
+        y = 999;
+        double division;
+        division = (double) x / (double) y; 
+        
+
+        System.out.printf("x + y = %d%n",x+y);
+        System.out.printf("x - y = %d%n",x-y);
+        System.out.printf( "x / y = %.8f%n",+ division);
+        System.out.printf("x * y = %d%n",x*y);
+
     }
 }
